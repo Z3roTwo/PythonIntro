923bdf79836dd0e3a468cbb9e120f3c44574000f
Jerker Ekman <jerker.ekman@elev.ga.ntig.se>
2019-09-10 19:51:03 +0200

    kalkylator 2.0

--- wallabaj.py
+++ wallabaj.py
@@ -45,26 +45,83 @@
 #
 #    print(Summa)
 
-memory = int(input("Value: "))
-i = True
-while i == True:
+#memory = int(input("Value: "))
+#i = True
+#while i == True:
+#    try:
+#        operator = int(input("Choose operator [1] + [2] - [3] * [4] / [5] =: "))
+#        tal = int(input("Value: "))
+#    except ValueError:
+#        print("That's not a number... Please enter a real number next time.")
+#
+#    if operator == 1:
+#        Summa = memory + tal
+#   elif operator == 2:
+#       Summa = memory - tal
+#    elif operator == 3:
+#       Summa = memory * tal
+#    elif operator == 4:
+#       Summa = memory / tal
+#   elif operator > 4:
+#       Summa = memory
+#        i = False
+#    memory = Summa
+#
+#    print(Summa)
+
+memory = int(input("Memory: ")) # första siffran
+prev = [0, 3, 0] # sparar föregående uträkning för att få gånger och delat att fungera på rätt sätt
+tal = 0 # bara för att hålla alla variabler vid toppen
+operator = 0 # bara för att hålla alla variabler vid toppen
+summa = 0 # bara för att hålla alla variabler vid toppen
+new = 0 # bara för att hålla alla variabler vid toppen
+loop = True # så länge den är true så fortsätter den fråga efter nya tal
+
+while loop == True:
     try:
         operator = int(input("Choose operator [1] + [2] - [3] * [4] / [5] =: "))
         tal = int(input("Value: "))
     except ValueError:
         print("That's not a number... Please enter a real number next time.")
 
-    if operator == 1:
-        Summa = memory + tal
-    elif operator == 2:
-        Summa = memory - tal
-    elif operator == 3:
-        Summa = memory * tal
-    elif operator == 4:
-        Summa = memory / tal
-    elif operator > 4:
-        Summa = memory
-        i = False
-    memory = Summa
+    if operator == 3:
 
-    print(Summa)
+        if prev[1] == 1:
+            new = prev[2] * tal
+            summa = prev[0] + new
+        elif prev[1] == 2:
+            new = prev[2] * tal
+            summa = prev[0] - new
+        elif prev[1] == 3:
+            summa = memory * tal
+        elif prev[1] == 4:
+            new = prev[2] * tal
+            summa = prev[0] / new
+
+    elif operator == 4:
+
+        if prev[1] == 1:
+            new = prev[2] / tal
+            summa = prev[0] + new
+        elif prev[1] == 2:
+            new = prev[2] / tal
+            summa = prev[0] - new
+        elif prev[1] == 3:
+            summa = memory / tal
+        elif prev[1] == 4:
+            summa = memory / tal
+
+    elif operator == 1:
+        summa = memory + tal
+    elif operator == 2:
+        summa = memory - tal
+    elif operator < 1 or operator > 4:
+        summa = memory
+        loop = False
+
+    prev[0] = memory
+    prev[1] = operator
+    prev[2] = tal
+    memory = summa
+
+    print(summa)
